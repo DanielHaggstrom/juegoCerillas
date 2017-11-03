3b341e0092969d6a22620e34f008798352c26dea
DanielHaggstrom <daniel.hpf@hotmail.com>
2017-11-03 20:03:29 +0100

    Create utilsWin.h

---
+++ utilsWin.h
@@ -0,0 +1,12 @@
+#ifndef UTILSWIN
+#define UTILSWIN
+
+#include <Windows.h>
+
+enum keys { UP = VK_UP, RIGHT = VK_RIGHT, LEFT = VK_LEFT, DOWN = VK_DOWN, ESCAPE = VK_ESCAPE, ENTER = VK_RETURN };
+
+void message(LPCSTR word);
+
+int getKey();
+
+#endif
