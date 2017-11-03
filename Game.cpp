0df56f6dc9deb8ce8f1673a998c349cbe8eab98c
DanielHaggstrom <daniel.hpf@hotmail.com>
2017-11-03 19:12:28 +0100

    moar changes, everything is still the same

--- Game.cpp
+++ Game.cpp
@@ -1,30 +1,21 @@
 #include "Game.h"
 
-class Game {
-	int cerillas; // numero de cerillas iniciales
-
-
-
-public:
-
-	int cerillasRestantes(int cerillas, int movimiento) {
+int Game::cerillasRestantes(int cerillas, int movimiento) {
 		return (cerillas - movimiento);
-	}
+}
 
-	Game() {
-		cerillas = 0; // numero defauls de cerillas
-	}
+Game::Game() {
+	cerillas = 0; // numero defauls de cerillas
+}
 
-	void funcionPrincipal(int cerillas) { // funcion donde ocurre el juego en si
-		int round = 0; // This is the current round that is being played. Initilize in 0 to start in 1.
-		while (cerillas > 0) {
-			round++;
-			/* 
-			player = getPlayer(round);
-			int movimiento = getMovimiento(player);
-			cerillas = cerillasRestantes(cerillas, movimiento);
-			*/
-
-		}
+void Game::funcionPrincipal(int cerillas) { // funcion donde ocurre el juego en si
+	int round = 0; // This is the current round that is being played. Initilize in 0 to start in 1.
+	while (cerillas > 0) {
+		round++;
+		/*
+		player = getPlayer(round);
+		int movimiento = getMovimiento(player);
+		cerillas = cerillasRestantes(cerillas, movimiento);
+		*/
 	}
-};
+}
