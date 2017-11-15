5d30477da6794375f454ea9f9fd7476b5155759e
vigonz06 <victorglez96@gmail.com>
2017-11-15 09:28:33 +0100

    dificultades

--- Game.cpp
+++ Game.cpp
@@ -12,6 +12,81 @@ void Game::actualizar(int move)
 	cerillas -= move;
 }
 
+int Game::digitoAutomata()
+{
+	return (rand() % 3) + 1;
+}
+
+int Game::botFacil()
+{
+	return digitoAutomata();
+}
+
+int Game::BotDificil(int restantes)
+{
+	if (restantes == 4) return botFacil();
+	
+	else
+	{
+		if (restantes < 4) return restantes;
+		
+		else
+		{
+			for(int i = 1; i <= 3)
+			{
+				if((restantes - i) == 4) return i;
+			}
+			return botFacil();
+		}
+	}
+}
+
+int Game::botImposible()
+{
+	int move = 0;
+	
+	for(int i = 1; i <= 3)
+	{
+		if(validMove(i))
+		{
+			if(minimax(i, cerillas, true)) return i;
+		}
+	}
+	return botDificil();
+}
+
+bool Game::minimax(int move, int total, bool turno)
+{
+	if(total <= 0) return turno;
+	
+	if(turno)
+	{
+		bool bestValue = false;
+		int i = 1;
+		
+		while(i <= 3 &&!bestValue)
+		{
+			if(validMove(i) bestValue = minimax(i, total - i, false);
+			
+			i++;
+		}
+		return bestValue;
+	}
+	if(!turno)
+	{
+		bool worstValue = true;
+		int j = 1;
+		
+		while(j <= 3 && worstValue)
+		{
+			if(validMove(i) worstValue = minimax(i, total - i, true);
+			
+			j++;
+		}
+		return worstValue;
+	}
+}
+
 void Game::Single(Game game) 
 { // funcion donde ocurre el juego en si
 	std::string user = GraphInter::get()->validUser("Jugador");
@@ -30,7 +105,7 @@ void Game::Single(Game game)
 			switch(nivel)
 			{
 				case Facil:
-					move = automove();
+					move = botFacil();
 					break;
 				case Dificil:
 					move = botDificil();
